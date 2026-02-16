ad1874f6953795cc7c36d2a36a7ca2e42a77aaee
BilionIevgeniy <bilionjenia1111@gmail.com>
2026-02-16 14:36:44 +0100

    first script added

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+const hamburger = document.getElementById("hamburger");
+const nav = document.querySelector(".main-nav");
+console.log(nav);
+const toggleMenu = () => {
+  const styles = window.getComputedStyle(nav);
+  if (styles.display === "block") {
+    nav.style.display = "none";
+    return;
+  }
+  nav.style.display = "block";
+};
+hamburger.addEventListener("click", toggleMenu);

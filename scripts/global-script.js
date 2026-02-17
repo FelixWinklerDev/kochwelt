67ba9f36636b65f18884500bb530b48ae59194c5
BilionIevgeniy <bilionjenia1111@gmail.com>
2026-02-17 10:41:35 +0100

    added animation to burger

--- scripts/global-script.js
+++ scripts/global-script.js
@@ -1,13 +1,7 @@
-const hamburger = document.getElementById("hamburger");
-const nav = document.querySelector(".main-nav");
-
-const toggleMenu = () => {
-  const styles = window.getComputedStyle(nav);
-  if (styles.display === "block") {
-    nav.style.display = "none";
-    return;
+function toggleMenu(button) {
+  button.classList.toggle("open");
+  const nav = document.querySelector(".header nav");
+  if (nav) {
+    nav.classList.toggle("hide");
   }
-  nav.style.display = "block";
-};
-
-hamburger.addEventListener("click", toggleMenu);
+}
